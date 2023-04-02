d9972a8a96b34d0610650399d376d1110d5b08af
NachoM57 <ignaciomaiorino50@gmail.com>
2023-04-01 22:48:36 -0300

    por la mitad del proyecto de prueba

---
+++ src/app/COMPONENTES/cafe-servicios/cafe-servicios.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cafe-servicios',
+  templateUrl: './cafe-servicios.component.html',
+  styleUrls: ['./cafe-servicios.component.css']
+})
+export class CafeServiciosComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BannerComponent } from './COMPONENTES/banner/banner.component';
+import { TiposCafesComponent } from './COMPONENTES/tipos-cafes/tipos-cafes.component';
+import { CafeServiciosComponent } from './COMPONENTES/cafe-servicios/cafe-servicios.component';
+import { BlogComponent } from './COMPONENTES/blog/blog.component';
+import { FooterComponent } from './COMPONENTES/footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    TiposCafesComponent,
+    CafeServiciosComponent,
+    BlogComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
